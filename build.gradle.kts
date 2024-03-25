71c114b67b2ec8b6fa402077630e3ffd6c250041
Lucy Poulton <lucy@lucypoulton.net>
2024-03-25 23:53:30 +0000

    Update to 1.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,21 @@ plugins {
     id("fabric-loom") version "1.1-SNAPSHOT"
 }
 
-version = "1.0.0-SNAPSHOT"
+version = "1.0.1"
 group = "net.lucypoulton"
 
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20")
+    minecraft("com.mojang:minecraft:1.20.4")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.14.21")
+    modImplementation("net.fabricmc:fabric-loader:0.15.7")
 
 }
 
 tasks.processResources {
+    filteringCharset = "UTF-8"
+
     filesMatching("fabric.mod.json") {
         expand("version" to project.version)
     }
--- src/main/java/net/lucypoulton/sillylittlemod/mixin/ScreenMixin.java
+++ src/main/java/net/lucypoulton/sillylittlemod/mixin/ScreenMixin.java
@@ -3,18 +3,12 @@ package net.lucypoulton.sillylittlemod.mixin;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
-    @Inject(
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;fillGradient(IIIIII)V"),
-            method = "renderBackground",
-            cancellable = true
-    )
-    public void renderBackground(GuiGraphics guiGraphics, CallbackInfo ci) {
-        ci.cancel();
+    @Overwrite
+    public void renderTransparentBackground(GuiGraphics graphics) {
+        // Do nothing
     }
 }
